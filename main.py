437d80d045b19758389d9d0d433845ff92165a1a
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-05-05 23:47:34 -0500

    12

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import streamlit as st
 import openai
 
-# Configurar el título de la aplicación de Streamlit
+# Configurar el título de la aplicación de Streamlit y la clave de API de OpenAI
 st.title('🤖 NachoBot')
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# Inicializar el estado de la sesión para almacenar mensajes y la historia de la conversación
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+# Inicializar el estado de la sesión para almacenar los mensajes de la conversación
 if "conversation_history" not in st.session_state:
     st.session_state.conversation_history = [
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "assistant", "content": "Hola, ¿cómo puedo ayudarte?"}
     ]
 
-# Configurar las claves de API de OpenAI usando el módulo secrets de Streamlit
-openai.api_key = st.secrets["OPENAI_API_KEY"]
-
 # Función para obtener respuestas del modelo de OpenAI
 def get_response(message):
+    st.session_state.conversation_history.append({"role": "user", "content": message})
     try:
-        st.session_state.conversation_history.append({"role": "user", "content": message})
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=st.session_state.conversation_history
@@ -30,17 +26,14 @@ def get_response(message):
     except Exception as e:
         return f"Ocurrió un error: {e}"
 
-# Captura y maneja la entrada del usuario
+# Interfaz de usuario para entrada de chat
 prompt = st.chat_input("¿Cómo puedo ayudarte?")
 if prompt:
     with st.chat_message("user"):
         st.markdown(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Obtener respuesta del modelo OpenAI
     response = get_response(prompt)
-
-    # Envía y muestra la respuesta del asistente
     with st.chat_message("assistant"):
         st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
